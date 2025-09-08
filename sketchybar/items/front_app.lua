c8b412eda545882e12fc30109e75d5f3ac51ab78
robsteriam <28790598+robsteriam@users.noreply.github.com>
2025-09-08 15:22:47 -0700

    Update front_app.lua

--- sketchybar/items/front_app.lua
+++ sketchybar/items/front_app.lua
@@ -1,12 +1,12 @@
-local colors = require("colors")
-local settings = require("settings")
+local fonts = require("fonts")
+local sbar = require("sketchybar")
 
 local front_app = sbar.add("item", "front_app", {
 	display = "active",
 	icon = { drawing = false },
 	label = {
 		font = {
-			style = settings.font.style_map["Black"],
+			style = fonts.font.style_map["Black"],
 			size = 12.0,
 		},
 	},
@@ -17,6 +17,6 @@ front_app:subscribe("front_app_switched", function(env)
 	front_app:set({ label = { string = env.INFO } })
 end)
 
-front_app:subscribe("mouse.clicked", function(env)
+front_app:subscribe("mouse.clicked", function()
 	sbar.exec("aerospace workspace next")
 end)
