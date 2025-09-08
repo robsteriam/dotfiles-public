b3c4626db0c80a69825ed94db0c91e3cc9fc68f4
robsteriam <28790598+robsteriam@users.noreply.github.com>
2025-09-08 15:18:55 -0700

    Update settings.lua, moved fonts settings to fonts.lua

--- sketchybar/settings.lua
+++ sketchybar/settings.lua
@@ -1,28 +1,4 @@
 return {
-	font = {
-		text = "SF Pro",
-		numbers = "SF Pro",
-		size = 15.0,
-		style_map = {
-			["Regular"] = "Regular",
-			["Semibold"] = "Semibold",
-			["Bold"] = "Bold",
-			["Heavy"] = "Heavy",
-			["Black"] = "Black",
-		},
-	},
-	font_icon = {
-		text = "Hack Nerd Font",
-		numbers = "Hack Nerd Font",
-		size = 15.0,
-		style_map = {
-			["Regular"] = "Regular",
-			["Semibold"] = "Semibold",
-			["Bold"] = "Bold",
-			["Heavy"] = "Heavy",
-			["Black"] = "Black",
-		},
-	},
 	height = 36,
 	paddings = 6,
 	group_paddings = 5,
