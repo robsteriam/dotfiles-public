3f79d34924669aa1355ffa2e65d075886b7ac294
robsteriam <28790598+robsteriam@users.noreply.github.com>
2025-09-08 15:25:19 -0700

    Update wifi.lua

--- sketchybar/items/widgets/wifi.lua
+++ sketchybar/items/widgets/wifi.lua
@@ -1,6 +1,8 @@
 local icons = require("icons")
-local colors = require("colors")
+local colors = require("appearance").colors
 local settings = require("settings")
+local sbar = require("sketchybar")
+local fonts = require("fonts")
 
 -- Execute the event provider binary which provides the event "network_update"
 -- for the network interface "en0", which is fired every 2.0 seconds.
@@ -17,15 +19,15 @@ local wifi_up = sbar.add("item", "widgets.wifi1", {
 	icon = {
 		padding_right = 0,
 		font = {
-			style = settings.font.style_map["Bold"],
+			style = fonts.font.style_map["Bold"],
 			size = 9.0,
 		},
 		string = icons.wifi.upload,
 	},
 	label = {
 		font = {
-			family = settings.font.numbers,
-			style = settings.font.style_map["Bold"],
+			family = fonts.font.numbers,
+			style = fonts.font.style_map["Bold"],
 			size = 9.0,
 		},
 		color = colors.red,
@@ -40,15 +42,15 @@ local wifi_down = sbar.add("item", "widgets.wifi2", {
 	icon = {
 		padding_right = 0,
 		font = {
-			style = settings.font.style_map["Bold"],
+			style = fonts.font.style_map["Bold"],
 			size = 9.0,
 		},
 		string = icons.wifi.download,
 	},
 	label = {
 		font = {
-			family = settings.font.numbers,
-			style = settings.font.style_map["Bold"],
+			family = fonts.font.numbers,
+			style = fonts.font.style_map["Bold"],
 			size = 9.0,
 		},
 		color = colors.blue,
@@ -76,7 +78,7 @@ local ssid = sbar.add("item", {
 	position = "popup." .. wifi_bracket.name,
 	icon = {
 		font = {
-			style = settings.font.style_map["Bold"],
+			style = fonts.font.style_map["Bold"],
 		},
 		string = icons.wifi.router,
 	},
@@ -85,7 +87,7 @@ local ssid = sbar.add("item", {
 	label = {
 		font = {
 			size = 15,
-			style = settings.font.style_map["Bold"],
+			style = fonts.font.style_map["Bold"],
 		},
 		max_chars = 18,
 		string = "????????????",
@@ -175,9 +177,9 @@ wifi_up:subscribe("network_update", function(env)
 	})
 end)
 
-wifi:subscribe({ "wifi_change", "system_woke" }, function(env)
-	sbar.exec("ipconfig getifaddr en0", function(ip)
-		local connected = not (ip == "")
+wifi:subscribe({ "wifi_change", "system_woke" }, function()
+	sbar.exec("ipconfig getifaddr en0", function(ipaddr)
+		local connected = not (ipaddr == "")
 		wifi:set({
 			icon = {
 				string = connected and icons.wifi.connected or icons.wifi.disconnected,
