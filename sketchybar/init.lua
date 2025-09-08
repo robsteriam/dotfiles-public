fa4e01c7f42695e3fb618b058e367c3ef31a01a7
robsteriam <28790598+robsteriam@users.noreply.github.com>
2025-09-08 15:17:48 -0700

    Update init.lua, updated references

--- sketchybar/init.lua
+++ sketchybar/init.lua
@@ -1,5 +1,5 @@
 -- Require the sketchybar module
-sbar = require("sketchybar")
+local sbar = require("sketchybar")
 
 -- Set the bar name, if you are using another bar instance than sketchybar
 -- sbar.set_bar_name("bottom_bar")
@@ -7,7 +7,7 @@ sbar = require("sketchybar")
 -- Bundle the entire initial configuration into a single message to sketchybar
 sbar.begin_config()
 require("bar")
-require("default")
+require("appearance")
 require("items")
 sbar.end_config()
 
