718ce579487c5a234f651cae3f20d5cee6f411d6
robsteriam <28790598+robsteriam@users.noreply.github.com>
2025-10-31 23:41:45 -0700

    Enhance apple item with click animation and subscription

--- config/sketchybar/items/apple.lua
+++ config/sketchybar/items/apple.lua
@@ -2,7 +2,7 @@ local icons = require("icons")
 local settings = require("settings")
 local sbar = require("sketchybar")
 
-sbar.add("item", {
+local apple = sbar.add("item", {
 	default = true,
 	icon = {
 		padding_left = settings.padding.icon_item.icon.padding_left,
@@ -11,5 +11,29 @@ sbar.add("item", {
 	},
 	label = { drawing = false },
 	click_script = "$CONFIG_DIR/helpers/menus/bin/menus -s 0",
-	subscribe = "logo mouse.clicked window_focus front_app_switched space_change title_change",
 })
+
+apple:subscribe("mouse.clicked", function()
+  sbar.animate("tanh", 8, function()
+    apple:set {
+      background = {
+        shadow = {
+          distance = 0,
+        },
+      },
+      y_offset = -4,
+      padding_left = 8,
+      padding_right = 8,
+    }
+    apple:set {
+      background = {
+        shadow = {
+          distance = 4,
+        },
+      },
+      y_offset = 0,
+      padding_left = 4,
+      padding_right = 4,
+    }
+  end)
+end)
